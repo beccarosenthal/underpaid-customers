5ce03891e52abac242fb2e8ef67b3fa873c22ff8
Becca Rosenthal <beccarosenthal@gmail.com>
2017-10-06 03:53:57 +0000

    doesn't work because floats are fucked up--HELP!

--- accounting.py
+++ accounting.py
@@ -32,18 +32,22 @@ if customer1_expected != customer1_paid:
     print customer1_name, "paid {:.2f}, expected {:.2f}".format(
         customer1_paid, customer1_expected)
 
-data = open("customer-orders.txt")
 
 def verify_customer_payment(text_file):
     """takes text file, returns info about customers who paid incorrectly"""
+
     melon_cost = 1.00
+    data = open(text_file)
 
     for line in data:
         line = line.rstrip()
         words = line.split('|')
-        customer_id, customer_name, product, amount_paid = words
-        print line
-     
+        customer_id, customer_name, num_melons, customer_paid = words
+
+        customer_expected = melon_cost * float(num_melons)
+        if customer_expected != customer_paid:
+            print customer_name, "paid {:.2f}, expected {:.2f}".format(customer_paid, customer_expected)
+
 
 # # def check_customer_paid_correctly(customer_name, quantity_purchased, amount_paid):
 # #     """if customer paid wrong amount of money, tells what they should ahve paid"""
@@ -57,6 +61,6 @@ def verify_customer_payment(text_file):
 # #         amount = words[2]
 #         pass
 
-verify_customer_payment(data)
+verify_customer_payment("customer-orders.txt")
 
 
